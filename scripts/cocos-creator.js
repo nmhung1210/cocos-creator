228ec4dff7e8c95fe86b87f8d24ca81f58f42d8f
Hung Nguyen <nmhung1210@gmail.com>
2020-09-10 08:09:34 +0700

    Bump version 2.4.2p Fixed cli

--- scripts/cocos-creator.js
+++ scripts/cocos-creator.js
@@ -9,7 +9,8 @@ const run = cmd => execSync(cmd, {
 });
 
 const creatorDir = resolve(__dirname, "..", "creator");
-const argv = yargs.usage('\nUsage: $0 [path] - Open cocos creator.')
+const argv = yargs
+
     .command('build <path>', ' - Build project by path', (yargs) => {
         yargs
             .positional('path', {
@@ -18,25 +19,24 @@ const argv = yargs.usage('\nUsage: $0 [path] - Open cocos creator.')
     }, (argv) => {
         const projectRoot = resolve(process.cwd(), argv.path);
         process.chdir(creatorDir);
-        run(`npx electron --no-sandbox . --nologin --path="${projectRoot}" --build="platform=${argv.platform};debug=false" `);
+        run(`npx electron --no-sandbox ${creatorDir} --nologin --path="${projectRoot}" --build="platform=${argv.platform};debug=false" `);
+    })
+    .usage('$0 [path]', ' - Open cocos creator.', yargs => {}, (argv) => {
+        if (argv.path) {
+            const projectRoot = resolve(process.cwd(), argv.path);
+            if (fs.existsSync(projectRoot) && fs.statSync(projectRoot).isDirectory) {
+                process.chdir(creatorDir);
+                run(`npx electron --no-sandbox ${creatorDir} --nologin --path="${projectRoot}"`);
+            } else {
+                console.error(`Project at ${projectRoot} does not exist!`);
+            }
+        } else {
+            process.chdir(creatorDir);
+            run(`npx electron --no-sandbox ${creatorDir} --nologin`);
+        }
     }).option('platform', {
         alias: 'p',
         type: 'string',
         default: 'web-mobile',
         description: 'Build platform <web-mobile|web-desktop|fb-instant-games|android|android-instant|ios|mac|win32>'
     }).argv;
-
-if (argv._.length === 1 && argv._[0]) {
-    const projectRoot = resolve(process.cwd(), argv._[0]);
-    if (fs.existsSync(projectRoot) && fs.statSync(projectRoot).isDirectory) {
-        process.chdir(creatorDir);
-        run(`npx electron --no-sandbox . --nologin --path="${projectRoot}"`);
-    } else {
-        console.error(`Project at ${projectRoot} does not exist!`);
-    }
-} else {
-    process.chdir(creatorDir);
-    run(`npx electron --no-sandbox . --nologin`);
-}
-
-
